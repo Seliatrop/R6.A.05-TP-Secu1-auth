26dbae21fcccae16d2290f220b1ea60c008a467a
Eroge_Sama <118463730+Seliatrop@users.noreply.github.com>
2024-02-13 16:52:16 +0100

    Etape 3

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,25 +1,52 @@
-import {createHash} from "node:crypto"
+// login.js
+import {createHash} from 'node:crypto';
 
-const users = []    // Simule BDD pour le stockage des utilisateurs
-const role = ['admin', 'utilisateur']
+const users = []; // Simule BDD pour le stockage des utilisateurs
+const roles = ['admin', 'utilisateur'];
 
 export const addUser = async (req, res) => {
-    const {email, password} = req.body
-    const hashedPassword = createHash("sha256").update(password).digest().toString("hex")
+    const {email, password} = req.body;
+    const hashedPassword = createHash('sha256').update(password).digest('hex');
 
-    let user = users.find((u) => u.email === email && u.password === hashedPassword)
+    // Vérifier si l'utilisateur existe déjà
+    let user = users.find((u) => u.email === email);
+
     if (user) {
         res.status(401).send({
-            message: "Utilisateur déjà enregistré",
-            user
-        })
-    }
+            message: 'Utilisateur déjà enregistré',
+            user,
+        });
+    } else {
+        // Ajouter un nouvel utilisateur
+        const role = roles[Math.floor(Math.random() * roles.length)]; // Choisissez un rôle au hasard
+        user = {email, password: hashedPassword, role};
+        users.push(user);
 
-    // A compléter
-}
+        res.status(200).send({
+            message: 'Utilisateur enregistré avec succès',
+            user,
+        });
+    }
+};
 
 export const loginUser = async function (req, res) {
+    const {email, password} = req.body;
+    const hashedPassword = createHash('sha256').update(password).digest('hex');
 
-    // A compléter
+    // Vérifier les informations de connexion
+    const user = users.find((u) => u.email === email && u.password === hashedPassword);
 
-}
+    if (user) {
+        // Générer un jeton JWT avec le rôle et l'email dans le payload
+        const token = sign({email: user.email, role: user.role}, secretKey, {algorithm: 'HS256'});
+
+        res.status(200).send({
+            message: 'Connexion réussie',
+            token,
+        });
+    } else {
+        res.status(401).send({
+            message: 'Utilisateur non-identifié',
+        });
+    }
+};
--- src/plugins/jwt.js
+++ src/plugins/jwt.js
@@ -3,9 +3,14 @@ import fastifyJwt from "@fastify/jwt";
 
 export default fp(async function (app, opts) {
 
+    const privateKey = fs.readFileSync('../../.ssl/private-key.pem', 'utf-8');
+
     app.register(fastifyJwt, {
+        secret: {
+            private: privateKey
+        },
+        algorithms: ['ES256'],
         sign: {
-            algorithm: 'ES256',
             issuer: 'info.iutparis.fr'
         },
     })
